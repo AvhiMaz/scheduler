77b5a9f33945559ca439c06bc02adb63af103a7a
AvhiMaz <avhimazumder5@outlook.com>
2026-03-06 16:29:10 +0530

    chore: define tx struct
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
-int main () {
-    printf("Hello, World!");
-        return 0;
+#define MAX_ACCOUNTS 64
+#define PUBKEY_SIZE 32
+
+#define TX_PENDING 0
+#define TX_RUNNING 1
+#define TX_DONE 2
+#define TX_FAILED 3
+
+typedef struct {
+    uint16_t id;
+    uint16_t priority_fee;
+    char accounts[MAX_ACCOUNTS][PUBKEY_SIZE];
+    int num_accounts;
+    bool is_writable[MAX_ACCOUNTS];
+    void (*execute)(void *);
+    void *args;
+    int status;
+} Transaction;
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
 };
