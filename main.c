d14645bef75674bb1c5f80c754207ff98fd5b826
AvhiMaz <avhimazumder5@outlook.com>
2026-03-06 16:36:51 +0530

    fmt
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- main.c
+++ main.c
@@ -3,25 +3,22 @@
 #include <stdio.h>
 
 #define MAX_ACCOUNTS 64
-#define PUBKEY_SIZE 32
+#define PUBKEY_SIZE  32
 
-#define TX_PENDING 0
-#define TX_RUNNING 1
-#define TX_DONE 2
-#define TX_FAILED 3
+#define TX_PENDING   0
+#define TX_RUNNING   1
+#define TX_DONE      2
+#define TX_FAILED    3
+
+typedef void (*ExecuteFn)(void *);
 
 typedef struct {
-    uint16_t id;
-    uint16_t priority_fee;
-    char accounts[MAX_ACCOUNTS][PUBKEY_SIZE];
-    int num_accounts;
-    bool is_writable[MAX_ACCOUNTS];
-    void (*execute)(void *);
-    void *args;
-    int status;
+    uint16_t  id;
+    uint16_t  priority_fee;
+    char      accounts[MAX_ACCOUNTS][PUBKEY_SIZE];
+    int       num_accounts;
+    bool      is_writable[MAX_ACCOUNTS];
+    int       status;
+    ExecuteFn execute;
+    void     *args;
 } Transaction;
-
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-};
