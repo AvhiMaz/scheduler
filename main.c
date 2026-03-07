bbb930819563ab1a40224e2fc7590af2f6b369b6
AvhiMaz <avhimazumder5@outlook.com>
2026-03-07 22:13:00 +0530

    feat: add pop fn
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- main.c
+++ main.c
@@ -50,3 +50,38 @@ void push_pq(PriorityQueue *pq, Transaction *tx) {
         }
     }
 }
+
+Transaction *pop_pq(PriorityQueue *pq) {
+    Transaction *popped = pq->data[0];
+
+    pq->data[0] = pq->data[pq->size - 1];
+
+    pq->size--;
+
+    int i = 0;
+
+    while (1) {
+        int left_child = (2 * i) + 1;
+        int right_child = (2 * i) + 2;
+
+        if (left_child >= pq->size)
+            break;
+
+        int largest = left_child;
+        if (right_child < pq->size && pq->data[right_child]->priority_fee >
+                                          pq->data[left_child]->priority_fee) {
+            largest = right_child;
+        }
+
+        if (pq->data[i]->priority_fee < pq->data[largest]->priority_fee) {
+            Transaction *temp = pq->data[i];
+            pq->data[i] = pq->data[largest];
+            pq->data[largest] = temp;
+            i = largest;
+        } else {
+            break;
+        }
+    }
+    return popped;
+}
+
