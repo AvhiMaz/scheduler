39e5172921c02dd155c3a42c3cdb77b5a175e503
AvhiMaz <avhimazumder5@outlook.com>
2026-03-07 22:16:07 +0530

    feat: add `main` fn
    this is just to test if `push` and `pop` is working
    or not, we definitely gonna write desire testcases,
    but i don't know how to write tests in C :sob
    
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- main.c
+++ main.c
@@ -85,3 +85,22 @@ Transaction *pop_pq(PriorityQueue *pq) {
     return popped;
 }
 
+int main() {
+    PriorityQueue pq;
+    pq.size = 0;
+
+    Transaction t1 = {.id = 1, .priority_fee = 50, .status = TX_PENDING};
+    Transaction t2 = {.id = 2, .priority_fee = 90, .status = TX_PENDING};
+    Transaction t3 = {.id = 3, .priority_fee = 30, .status = TX_PENDING};
+
+    push_pq(&pq, &t1);
+    push_pq(&pq, &t2);
+    push_pq(&pq, &t3);
+
+    while (pq.size > 0) {
+        Transaction *tx = pop_pq(&pq);
+        printf("id=%d fee=%d\n", tx->id, tx->priority_fee);
+    }
+
+    return 0;
+}
