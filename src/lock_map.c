ae16e00b58aa21c6168a0a8305f344265ce797e5
AvhiMaz <avhimazumder5@outlook.com>
2026-03-08 15:18:25 +0530

    feat: add `lm_lock` fn
    this fn will update the status of a tx and lock it
    where free = 0 and lock = 1, this also uses chaining
    to avoid collision (when two node have same key)
    
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- src/lock_map.c
+++ src/lock_map.c
@@ -1,4 +1,6 @@
 #include "lock_map.h"
+#include <stdlib.h>
+#include <string.h>
 
 int hash(char *key) {
 
@@ -8,4 +10,27 @@ int hash(char *key) {
     };
 
     return sum % LOCK_MAP_SIZE;
+}
+
+int lm_lock(LockedHash *lm, char *key) {
+    int       index = hash(key);
+
+    LockNode *node = lm->buckets[index];
+
+    while (node != NULL) {
+        if (strcmp(node->key, key) == 0) {
+            node->status = 1;
+            return 1;
+        }
+
+        node = node->next;
+    }
+
+    LockNode *new_node = malloc(sizeof(LockNode));
+    memcpy(new_node->key, key, 32);
+    new_node->status = 1;
+    new_node->next = lm->buckets[index];
+    lm->buckets[index] = new_node;
+
+    return 1;
 }
