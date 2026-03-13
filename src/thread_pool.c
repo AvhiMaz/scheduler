05270280d0916e435f3ea1fbcf41f35138ed655a
AvhiMaz <avhimazumder5@outlook.com>
2026-03-13 20:38:33 +0530

    feat: impl `tp_submit` fn()
    basically this gonna take the tx to the `PriorityQueue`
    
    Signed-off-by: AvhiMaz<avhimazumder5@outlook.com>

--- src/thread_pool.c
+++ src/thread_pool.c
@@ -1,6 +1,8 @@
 #include "thread_pool.h"
 #include "defines.h"
+#include "priority_queue.h"
 #include "transaction.h"
+#include <pthread.h>
 
 void *worker(void *args) {
     ThreadPool *tp = (ThreadPool *)args;
@@ -33,4 +35,12 @@ void tp_init(ThreadPool *tp) {
     for (int i = 0; i < MAX_THREAD_POOL_SIZE; i++) {
         pthread_create(&tp->thread[i], NULL, worker, tp);
     }
+}
+
+void tp_submit(ThreadPool *tp, Transaction *tx) {
+
+    pthread_mutex_lock(&tp->mutex);
+    push_pq(&tp->queue, tx);
+    pthread_cond_signal(&tp->cond);
+    pthread_mutex_unlock(&tp->mutex);
 }
